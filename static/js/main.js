4dd1825d88ea0918f14f0ed997c113f680a5586e
allisonlu <allisonlu820@gmail.com>
2016-05-12 16:05:06 -0700

    highlight pls

--- static/js/main.js
+++ static/js/main.js
@@ -2,8 +2,8 @@ function navHighlight()
 {
     //  Gets the pathname of current URL, omitting the leading forward slash
     var active = window.location.pathname;
-    active = active.slice(1);
-    //  console.log(active);
+    active = active.slice(14);
+    console.log(active);
 
 
     //  If pathname is '', home gets hard coded active class
@@ -11,27 +11,27 @@ function navHighlight()
     //  does not match the ID they have.
     //  the cases with pages with matching IDs and pathnames are caught at the end
 
-    if (active === "")
+    if (active == "index.php")
     {
             // document.getElementById("home").setAttribute("class", "active");
         //  console.log("hit home case");
         document.getElementById("home").style.borderTopColor = "#4B90E0";
     }
 
-    else if (active == "support" || active == "testing")
+    else if (active == "support.php" || active == "testing.php")
     {
         //  console.log("before services");
         // document.getElementById("services").setAttribute("class", "active");
         document.getElementById("services").style.borderTopColor = "#4B90E0";
     }
 
-    else if (active == "aboutHIV" || active == "prevention" || active == "videos")
+    else if (active == "aboutHIV.php" || active == "prevention.php" || active == "videos.php")
     {
         document.getElementById("information").style.borderTopColor = "#4B90E0";
         // document.getElementById("information").setAttribute("class", "active");
         //  console.log("hit info");
     }
-    else if (active == "partners" || active == "advocacy")
+    else if (active == "partners.php" || active == "advocacy.php")
     {
         document.getElementById("community").style.borderTopColor = "#4B90E0";
     }
