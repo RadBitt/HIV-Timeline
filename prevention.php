b136698c1b9d6855d7bf572731d85af15c2d263c
Lutalo <wongstevenj@gmail.com>
2016-05-16 14:34:23 -0700

    changed images on prevention page to link to their brochure

--- prevention.php
+++ prevention.php
@@ -14,13 +14,18 @@
 	<div class="prevention container">
 		<div class="content col-md-4">
 			<div class="brochure">
+				<a href="http://www.ccsf.edu/dam/Organizational_Assets/Department/Health_Physical_Education/Health_Education_Community_Health_Studies/HIV_STI_Prevention_Ed/HIV_STI_Prevent_PDF/F12.24%20HIV%20Pamphlet.pdf">
 				<img src="http://www.w3schools.com/bootstrap/sanfran.jpg">
+				</a>
+	
 				<h4>HIV: HIV Treatment is HIV Prevention</h4>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 			</div>
 
 			<div class="brochure">
+				<a href="http://www.ccsf.edu/dam/Organizational_Assets/Department/Health_Physical_Education/Health_Education_Community_Health_Studies/HIV_STI_Prevention_Ed/HIV_STI_Prevent_PDF/F12.24%20Safer%20Sex%20Pamphlet.pdf">
 				<img src="http://www.w3schools.com/bootstrap/sanfran.jpg">
+				</a>
 				<h4>Safer Sex Self Reflection</h4>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 			</div>
@@ -33,13 +38,17 @@
         </div>
    	 	<div class="content col-md-4">
 			<div class="brochure">
+				<a href="http://www.ccsf.edu/dam/Organizational_Assets/Department/Health_Physical_Education/Health_Education_Community_Health_Studies/HIV_STI_Prevention_Ed/HIV_STI_Prevent_PDF/F12.24%20STI%20Pamphlet.pdf">
 				<img src="http://www.w3schools.com/bootstrap/sanfran.jpg">
+				</a>
 				<h4>Sexually Transmitted Infection (STI)</h4>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 			</div>
 
 			<div class="brochure">
+				<a href="http://www.ccsf.edu/dam/Organizational_Assets/Department/Health_Physical_Education/Health_Education_Community_Health_Studies/HIV_STI_Prevention_Ed/HIV_STI_Prevent_PDF/F12.24%20Harm%20Reduction%20Pamphlet.pdf">
 				<img src="http://www.w3schools.com/bootstrap/sanfran.jpg">
+				</a>
 				<h4>Harm Reduciton: Risk Prevention</h4>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 			</div>
