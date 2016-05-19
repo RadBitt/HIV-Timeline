2d93948831ae2de3704b03fb326819b2166afbe9
Lutalo <wongstevenj@gmail.com>
2016-05-19 16:09:05 -0700

    e1107dd there is no cure, just curses

--- prevention.php
+++ prevention.php
@@ -12,7 +12,7 @@
 	</div>
 
 	<div class="prevention container">
-		<div class="content col-md-3">
+		<div class="content col-md-4">
 			<div class="brochure">
 				<a href="static/img/brochures/hiv-treatment-prevention.pdf">
 				<img src="static/img/brochures/HIV.thumb.png">
@@ -30,27 +30,6 @@
 				<a href="static/img/brochures/safer-sex-pamphlet.pdf">
 				<img src="static/img/brochures/hepatitis.thumb.png">
 				</a>
-<<<<<<< HEAD
-				<h4>Hepatitis: Get the Real Facts Brochure</h4>
-			</div>
-      </div>
-
-   	 	<div class="content col-md-3">
-				<div class="brochure">
-					<a href="sti-pamphlet.pdf">
-					<img src="static/img/brochures/STI.thumb.png">
-					</a>
-					<h4>Sexually Transmitted Infection (STI)</h4>
-				</div>
-
-				<div class="brochure">
-					<a href="harm-reduction-pamphlet.pdf">
-					<img src="static/img/brochures/harm.thumb.png">
-					</a>
-					<h4>Harm Reduciton: Risk Prevention</h4>
-				</div>
-=======
-<<<<<<< HEAD
 				<h4><a href="static/img/brochures/hepatitis-pamphlet.pdf">Hepatitis: Get the Real Facts Brochure</a></h4>
 			</div>		
 
@@ -68,15 +47,12 @@
 				<img src="static/img/brochures/harm.thumb.png">
 				</a>
 				<h4><a href="harm-reduction-pamphlet.pdf">Harm Reduciton: Risk Prevention</a></h4>
->>>>>>> origin/master
 			</div>
-
-			<div class="col-md-1 col-sm-12"></div>
-
-		<div class="col-md-3 col-sm-12">
+		</div>
+		<div class="col-md-3">
 			<div class="more-services">
 				<ul>More Services
-					<li><a href="support<?php echo $ext ?>">Community Support</a></li>
+					<li><a href="support.php">Community Support</a></li>
 				</ul>
 			</div>
 
